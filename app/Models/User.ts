449af3e9317c9606fe5d59faffab49d2088bce49
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-13 21:44:34 +0100

    fix(users): update id to string

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -4,7 +4,7 @@ import { column, beforeSave, BaseModel } from '@ioc:Adonis/Lucid/Orm'
 
 export default class User extends BaseModel {
   @column({ isPrimary: true })
-  public id: number
+  public id: string
 
   @column()
   public email: string
@@ -22,7 +22,7 @@ export default class User extends BaseModel {
   public updatedAt: DateTime
 
   @beforeSave()
-  public static async hashPassword (user: User) {
+  public static async hashPassword(user: User) {
     if (user.$dirty.password) {
       user.password = await Hash.make(user.password)
     }
