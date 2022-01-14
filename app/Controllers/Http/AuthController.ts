d9fe9427322268502d120c63ebaefba19a3064cf
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-14 09:09:41 +0100

    feat(auth): create user validator

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,9 +1,14 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import RegisterValidator from 'App/Validators/RegisterValidator'
 
 export default class AuthController {
   public async showRegisterForm({ view }: HttpContextContract) {
     return view.render('auth/register')
   }
 
-  public async register({}: HttpContextContract) {}
+  public async register({ request, response }: HttpContextContract) {
+    const payload = await request.validate(RegisterValidator)
+    // console.log(payload.email)
+    return response.redirect('/')
+  }
 }
