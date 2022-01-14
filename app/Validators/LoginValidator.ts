4e014da370dfc82b69632a08ec0dd99c91dbbdac
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-14 19:29:23 +0100

    fix(auth): update userActive rule

--- app/Validators/LoginValidator.ts
+++ app/Validators/LoginValidator.ts
@@ -1,17 +1,20 @@
 import { schema, rules } from '@ioc:Adonis/Core/Validator'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { UserStatus } from 'App/Enums/UserStatus'
 
 export default class LoginValidator {
   constructor(protected ctx: HttpContextContract) {}
 
   public schema = schema.create({
-    email: schema.string({ trim: true }, [rules.userActive()]),
+    email: schema.string({ trim: true }, [
+      rules.exists({ table: 'users', column: 'email', where: { status: UserStatus.Active } }),
+    ]),
     password: schema.string(),
   })
 
   public messages = {
     'email.required': "L'email est requis",
-    'email.userActive': "Cet email n'est pas vérifié",
+    'email.exists': "Cet email n'est pas vérifié",
     'password.required': 'Le mot de passe est requis',
   }
 }
