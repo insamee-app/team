0db5df4295ccf0e9aedb1218657e6d1cc5164204
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-15 11:27:01 +0100

    feat(school): create migration

---
+++ database/migrations/1642242233108_schools.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Schools extends BaseSchema {
+  protected tableName = 'schools'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+      table.string('name', 255).notNullable()
+      table.string('host', 255).notNullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
