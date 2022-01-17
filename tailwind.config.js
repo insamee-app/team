7990d4491cc92fb9fe8bb2a9b99fdf1b145dfb5a
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-17 23:43:19 +0100

    feat(profile): update design

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,11 @@ module.exports = {
             base: '#931f1c',
             dark: '#5f0000',
             light: '#c95144',
+            grey: {
+              base: '#c7bebd',
+              dark: '#584443',
+              light: '#e2dede',
+            },
           },
           secondary: {
             base: '#3ba99c',
