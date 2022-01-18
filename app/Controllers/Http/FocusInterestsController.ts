85c12845f8a6a5d3d067fc950954f51667127d6a
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-18 23:38:38 +0100

    feat(focus): create resource

--- app/Controllers/Http/FocusInterestsController.ts
+++ app/Controllers/Http/FocusInterestsController.ts
@@ -1,17 +1,70 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import FocusInterest from 'App/Models/FocusInterest'
+import FocusInterestStoreValidator from 'App/Validators/FocusInterestStoreValidator'
+import FocusInterestUpdateValidator from 'App/Validators/FocusInterestUpdateValidator'
 
 export default class FocusInterestsController {
-  public async index({}: HttpContextContract) {}
+  public async index({ view, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('viewList')
 
-  public async create({}: HttpContextContract) {}
+    const focusInterests = await FocusInterest.query().orderBy('name', 'asc')
 
-  public async store({}: HttpContextContract) {}
+    return view.render('pages/focus-interests/index', { focusInterests })
+  }
 
-  public async show({}: HttpContextContract) {}
+  public async create({ view, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('create')
 
-  public async edit({}: HttpContextContract) {}
+    return view.render('pages/focus-interests/create')
+  }
 
-  public async update({}: HttpContextContract) {}
+  public async store({ response, request, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('create')
 
-  public async destroy({}: HttpContextContract) {}
+    const data = await request.validate(FocusInterestStoreValidator)
+
+    const focusInterest = await FocusInterest.create(data)
+
+    return response.redirect().toRoute('FocusInterestsController.show', { id: focusInterest.id })
+  }
+
+  public async show({ view, params, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('view')
+
+    const focusInterest = await FocusInterest.findOrFail(params.id)
+
+    return view.render('pages/focus-interests/show', { focusInterest })
+  }
+
+  public async edit({ view, params, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('update')
+
+    const focusInterest = await FocusInterest.findOrFail(params.id)
+
+    return view.render('pages/focus-interests/edit', { focusInterest })
+  }
+
+  public async update({ request, response, params, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('update')
+
+    const focusInterest = await FocusInterest.findOrFail(params.id)
+
+    const data = await request.validate(FocusInterestUpdateValidator)
+
+    focusInterest.merge(data)
+
+    await focusInterest.save()
+
+    return response.redirect().toRoute('FocusInterestsController.show', { id: focusInterest.id })
+  }
+
+  public async destroy({ params, response, bouncer }: HttpContextContract) {
+    await bouncer.with('FocusInterestPolicy').authorize('delete')
+
+    const focusInterest = await FocusInterest.findOrFail(params.id)
+
+    await focusInterest.delete()
+
+    return response.redirect().toRoute('FocusInterestsController.index')
+  }
 }
