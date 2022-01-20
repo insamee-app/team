775a3f5c8d0b3e1d2fa37eecb7e994b3138ddeb9
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-20 19:50:07 +0100

    feat(profiles): add a maxLength to array

--- app/Validators/ProfileValidator.ts
+++ app/Validators/ProfileValidator.ts
@@ -9,10 +9,10 @@ export default class ProfileValidator {
     lastName: schema.string({ trim: true }),
     graduationYear: schema.number.optional(),
     skills: schema.array
-      .optional()
+      .optional([rules.maxLength(10)])
       .members(schema.string({}, [rules.exists({ table: 'skills', column: 'id' })])),
     focusInterests: schema.array
-      .optional()
+      .optional([rules.maxLength(10)])
       .members(schema.string({}, [rules.exists({ table: 'focus_interests', column: 'id' })])),
   })
 
@@ -20,8 +20,10 @@ export default class ProfileValidator {
     'firstName.required': 'Un prénom est requis',
     'lastName.required': 'Un nom est requis',
     'graduationYear.required': 'Un année de graduation est requise',
+    'skills.maxLength': 'Vous ne pouvez pas ajouter plus de 10 compétences',
     'skills.*.string': 'Une compétence doit être une chaîne de caractères',
     'skills.*.exists': "Une compétence n'existe pas",
+    'focusInterests.maxLength': "Vous ne pouvez pas ajouter plus de 10 centres d'intérêts",
     'focusInterests.*.string': "Un centre d'intérêt doit être une chaîne de caractères",
     'focusInterests.*.exists': "Un centre d'intérêt n'existe pas",
   }
