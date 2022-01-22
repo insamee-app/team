a8fea0abb3778248471c05a54aab2963baf3737e
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-22 17:01:24 +0100

    feat(thematic): create policy

---
+++ app/Policies/ThematicPolicy.ts
@@ -0,0 +1,31 @@
+import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
+import { UserRole } from 'App/Enums/UserRole'
+import User from 'App/Models/User'
+
+export default class ThematicPolicy extends BasePolicy {
+  public async before(user: User) {
+    if (user.role === UserRole.Admin) {
+      return true
+    }
+  }
+
+  public async viewList() {
+    return true
+  }
+
+  public async view() {
+    return true
+  }
+
+  public async create() {
+    return false
+  }
+
+  public async update() {
+    return false
+  }
+
+  public async delete() {
+    return false
+  }
+}
