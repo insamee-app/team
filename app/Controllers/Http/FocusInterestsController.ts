c3a608cd45b5501a1e0f855f804880e6c58477b4
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-01-27 19:01:04 +0100

    feat(focus): add list of mee

--- app/Controllers/Http/FocusInterestsController.ts
+++ app/Controllers/Http/FocusInterestsController.ts
@@ -1,9 +1,13 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
 import FocusInterest from 'App/Models/FocusInterest'
+import User from 'App/Models/User'
 import FocusInterestStoreValidator from 'App/Validators/FocusInterestStoreValidator'
 import FocusInterestUpdateValidator from 'App/Validators/FocusInterestUpdateValidator'
 
 export default class FocusInterestsController {
+  private PER_PAGE = 10
+
   public async index({ view, bouncer }: HttpContextContract) {
     await bouncer.with('FocusInterestPolicy').authorize('viewList')
 
@@ -28,12 +32,31 @@ export default class FocusInterestsController {
     return response.redirect().toRoute('FocusInterestsController.show', { id: focusInterest.id })
   }
 
-  public async show({ view, params, bouncer }: HttpContextContract) {
+  public async show({ request, view, params, bouncer }: HttpContextContract) {
     await bouncer.with('FocusInterestPolicy').authorize('view')
 
-    const focusInterest = await FocusInterest.findOrFail(params.id)
+    const page = request.input('page')
 
-    return view.render('pages/focus-interests/show', { focusInterest })
+    const focusInterest = await FocusInterest.findOrFail(params.id)
+    const users = await User.query()
+      .whereIn(
+        'id',
+        Database.from('profiles')
+          .select('profiles.user_id')
+          .join(
+            'focus_interests_profiles',
+            'focus_interests_profiles.profile_id',
+            '=',
+            'profiles.id'
+          )
+          .where('focus_interest_id', params.id)
+      )
+      .preload('profile', (profile) =>
+        profile.preload('focusInterests', (focusInterest) => focusInterest.groupLimit(3))
+      )
+      .paginate(page, this.PER_PAGE)
+
+    return view.render('pages/focus-interests/show', { focusInterest, users })
   }
 
   public async edit({ view, params, bouncer }: HttpContextContract) {
