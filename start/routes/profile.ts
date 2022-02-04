7a4ca54ca15d5d6ad21f729247c25ea8adcd6427
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-04 18:10:25 +0100

    feat(report): rework routes

--- start/routes/profile.ts
+++ start/routes/profile.ts
@@ -6,4 +6,7 @@ Route.group(() => {
   Route.get('mee/:id/avatars/edit', 'ProfilesAvatarsController.edit')
   Route.put('mee/:id/avatars', 'ProfilesAvatarsController.update')
   Route.delete('mee/:id/avatars', 'ProfilesAvatarsController.destroy')
+
+  Route.get('mee/:id/reports/create', 'ReportsProfilesController.createReport')
+  Route.post('mee/:id/reports', 'ReportsController.store')
 }).middleware('auth')
