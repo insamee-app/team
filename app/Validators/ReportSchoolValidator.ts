2863ce91f162dfb6bea84532cc2f456eaf63cdec
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-07 12:18:42 +0100

    fix(report): handle radio value when form issue

--- app/Validators/ReportSchoolValidator.ts
+++ app/Validators/ReportSchoolValidator.ts
@@ -6,13 +6,12 @@ export default class ReportSchoolValidator {
 
   public schema = schema.create({
     reasonId: schema.string({}, [rules.uuid({ version: 4 })]),
-    description: schema.string({}, [rules.maxLength(1024)]),
+    description: schema.string.optional({}, [rules.maxLength(1024)]),
   })
 
   public messages = {
     'reasonId.required': 'Veuillez sélectionner une raison',
     'reasonId.uuid': 'Veuillez sélectionner une raison valide',
-    'description.required': 'Veuillez saisir une description',
     'description.maxLength': 'La description doit faire moins de 1024 caractères',
   }
 }
