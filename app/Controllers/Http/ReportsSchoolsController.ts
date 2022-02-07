0120c91d275136054c2eee1c189e34b5ae846815
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-07 12:14:44 +0100

    feat(report): update report school controller

--- app/Controllers/Http/ReportsSchoolsController.ts
+++ app/Controllers/Http/ReportsSchoolsController.ts
@@ -3,27 +3,32 @@ import { ReasonType } from 'App/Enums/ReasonType'
 import { ReportEntity } from 'App/Enums/ReportEntity'
 import Reason from 'App/Models/Reason'
 import Report from 'App/Models/Report'
+import ReportSchoolValidator from 'App/Validators/ReportSchoolValidator'
 
 export default class ReportsSchoolsController {
-  // Ajouter le bouncer
+  public async create({ view, bouncer }: HttpContextContract) {
+    await bouncer.with('ReportSchoolPolicy').authorize('create')
 
-  public async create({ view }: HttpContextContract) {
     const reasons = await Reason.query().where('type', ReasonType.School)
 
     return view.render('pages/schools/reports/create', { reasons })
   }
 
-  public async store({ response, params, request, auth }: HttpContextContract) {
-    // Mettre en place le validator
-    const { reasonId, description } = request.all()
+  public async store({ response, params, request, auth, bouncer }: HttpContextContract) {
+    await bouncer.with('ReportSchoolPolicy').authorize('create')
+
+    const { reasonId, description } = await request.validate(ReportSchoolValidator)
 
     const report = await Report.query()
       .where('reporterId', auth.user!.id)
       .where('entityId', params.id)
       .whereNull('resolvedAt')
-      .first() // A utiliser pour le "vous avez déjà signalé"
+      .first()
 
-    // Si on a un report alors on met un message et on redirige vers la page de l'association
+    if (report) {
+      // TODO: Afficher un message d'erreur
+      return response.redirect().toRoute('SchoolsController.show', { id: params.id })
+    }
 
     await Report.create({
       reporterId: auth.user!.id,
