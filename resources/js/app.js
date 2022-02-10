2be813f9bd38f811d2e695117c3cbac35611cdf7
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-10 14:08:37 +0100

    feat(flash): create flash components

--- resources/js/app.js
+++ resources/js/app.js
@@ -185,4 +185,20 @@ Alpine.data('select', (data, selectedData = '') => ({
   },
 }))
 
+Alpine.data('flash', () => ({
+  open: false,
+  timeout: null,
+  // Automatically close after 5 seconds
+  init() {
+    this.open = true
+    this.timeout = setTimeout(() => {
+      this.open = false
+    }, 5000)
+  },
+  close() {
+    clearTimeout(this.timeout)
+    this.open = false
+  },
+}))
+
 Alpine.start()
