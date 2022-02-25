107f77a1ecd618aeaa7035093c66fc7b1f8149b9
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-25 20:38:05 +0100

    feat(events): create migration and model

---
+++ app/Models/Event.ts
@@ -0,0 +1,75 @@
+import { DateTime } from 'luxon'
+import { BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import AppSoftDeletes from './AppSoftDeletes'
+import { attachment, AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
+import User from './User'
+import Association from './Association'
+import School from './School'
+
+export default class Event extends AppSoftDeletes {
+  @column({ isPrimary: true })
+  public id: string
+
+  @column()
+  public name: string
+
+  @column()
+  public description: string
+
+  @column()
+  public status: number
+
+  @column()
+  public type: number
+
+  @column()
+  public location?: string
+
+  @column()
+  public startAt: DateTime
+
+  @column()
+  public endAt: DateTime
+
+  @column()
+  public ticketUrl?: string
+
+  @column()
+  public url?: string
+
+  @column()
+  public organizerAssociationId?: string
+
+  @column()
+  public organizerSchoolId?: string
+
+  @column()
+  public creatorId: string
+
+  @attachment({ folder: 'events', preComputeUrl: true })
+  public image: AttachmentContract | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+
+  @belongsTo(() => User, {
+    localKey: 'id',
+    foreignKey: 'creatorId',
+  })
+  public creator: BelongsTo<typeof User>
+
+  @belongsTo(() => Association, {
+    localKey: 'id',
+    foreignKey: 'organizerAssociationId',
+  })
+  public organizerAssociation: BelongsTo<typeof Association>
+
+  @belongsTo(() => School, {
+    localKey: 'id',
+    foreignKey: 'organizerSchoolId',
+  })
+  public organizerSchool: BelongsTo<typeof School>
+}
---
+++ database/migrations/1645814733275_events.ts
@@ -0,0 +1,35 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Events extends BaseSchema {
+  protected tableName = 'events'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
+      table.string('name').notNullable()
+      table.string('description').notNullable()
+      table.integer('status').unsigned().notNullable()
+      table.integer('type').unsigned().notNullable()
+      table.string('location').nullable()
+      table.timestamp('start_at', { useTz: true }).notNullable()
+      table.timestamp('end_at', { useTz: true }).notNullable()
+      table.string('ticket_url').nullable()
+      table.string('url').nullable()
+
+      table.uuid('organizer_association_id').nullable().references('id').inTable('associations')
+      table.uuid('organizer_school_id').nullable().references('id').inTable('schools')
+      table.uuid('creator_id').notNullable().references('id').inTable('users')
+
+      table.json('image').nullable()
+
+      table.timestamp('deleted_at', { useTz: true })
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
