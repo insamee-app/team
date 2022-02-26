5a483b111c190972247e6c7754f448e7b3174657
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-26 21:55:37 +0100

    feat(events): add routes for reports and images

--- start/routes/events.ts
+++ start/routes/events.ts
@@ -15,4 +15,11 @@ Route.group(() => {
     'update',
     'destroy',
   ])
+
+  Route.get('events/:id/images/edit', 'EventsImagesController.edit')
+  Route.put('events/:id/images', 'EventsImagesController.update')
+  Route.delete('events/:id/images', 'EventsImagesController.destroy')
+
+  Route.get('events/:id/reports/create', 'ReportsEventsController.create')
+  Route.post('events/:id/reports', 'ReportsEventsController.store')
 }).middleware('auth')
