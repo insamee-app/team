89afc6487d2698d80ddb68c5366e6d42ef439607
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-02-27 02:11:26 +0100

    feat(role): create new roles

--- app/Enums/UserRole.ts
+++ app/Enums/UserRole.ts
@@ -1,5 +1,13 @@
 export enum UserRole {
   Member = 1,
+  EventCreator,
+  EventManager,
+  SuperEventManager,
+  AssociativeManager,
+  SuperAssociativeManager,
+  Supervisor,
+  SuperSupervisor,
   Moderator,
   Admin,
+  SuperAdmin,
 }
