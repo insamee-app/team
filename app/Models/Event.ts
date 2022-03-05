c2119c813ee5a7b13afcad639cb40d9ebdaffd9a
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-03-05 10:21:25 +0100

    fix(events): udpate model

--- app/Models/Event.ts
+++ app/Models/Event.ts
@@ -1,11 +1,13 @@
 import { attachment, AttachmentContract } from '@ioc:Adonis/Addons/AttachmentLite'
 import { Filterable } from '@ioc:Adonis/Addons/LucidFilter'
 import { compose } from '@ioc:Adonis/Core/Helpers'
-import { BelongsTo, belongsTo, column } from '@ioc:Adonis/Lucid/Orm'
+import { BelongsTo, belongsTo, column, ManyToMany, manyToMany } from '@ioc:Adonis/Lucid/Orm'
+import { EventProfileState } from 'App/Enums/EventProfileState'
 import { DateTime } from 'luxon'
 import AppSoftDeletes from './AppSoftDeletes'
 import Association from './Association'
 import EventFilter from './Filters/EventFilter'
+import Profile from './Profile'
 import School from './School'
 import User from './User'
 
@@ -43,12 +45,6 @@ export default class Event extends compose(AppSoftDeletes, Filterable) {
   public url?: string
 
   @column()
-  public organizerAssociationId?: string
-
-  @column()
-  public organizerSchoolId?: string
-
-  @column()
   public creatorId: string
 
   @attachment({ folder: 'events', preComputeUrl: true })
@@ -66,15 +62,54 @@ export default class Event extends compose(AppSoftDeletes, Filterable) {
   })
   public creator: BelongsTo<typeof User>
 
-  @belongsTo(() => Association, {
+  @manyToMany(() => Association, {
+    pivotTable: 'events_associations',
+    pivotForeignKey: 'event_id',
+    pivotRelatedForeignKey: 'association_id',
+    relatedKey: 'id',
     localKey: 'id',
-    foreignKey: 'organizerAssociationId',
   })
-  public organizerAssociation: BelongsTo<typeof Association>
+  public organizingAssociations: ManyToMany<typeof Association>
 
-  @belongsTo(() => School, {
+  @manyToMany(() => School, {
+    pivotTable: 'events_schools',
+    pivotForeignKey: 'event_id',
+    pivotRelatedForeignKey: 'school_id',
+    relatedKey: 'id',
     localKey: 'id',
-    foreignKey: 'organizerSchoolId',
   })
-  public organizerSchool: BelongsTo<typeof School>
+  public organizingSchools: ManyToMany<typeof School>
+
+  @manyToMany(() => School, {
+    pivotTable: 'events_hosts',
+    pivotForeignKey: 'event_id',
+    pivotRelatedForeignKey: 'school_id',
+    relatedKey: 'id',
+    localKey: 'id',
+  })
+  public hostSchools: ManyToMany<typeof School>
+
+  @manyToMany(() => Profile, {
+    pivotTable: 'profiles_events',
+    pivotForeignKey: 'event_id',
+    pivotRelatedForeignKey: 'profile_id',
+    relatedKey: 'id',
+    localKey: 'id',
+    onQuery: (query) => {
+      query.where('state', EventProfileState.Interested)
+    },
+  })
+  public interestedProfiles: ManyToMany<typeof Profile>
+
+  @manyToMany(() => Profile, {
+    pivotTable: 'profiles_events',
+    pivotForeignKey: 'event_id',
+    pivotRelatedForeignKey: 'profile_id',
+    relatedKey: 'id',
+    localKey: 'id',
+    onQuery: (query) => {
+      query.where('state', EventProfileState.Participate)
+    },
+  })
+  public participatingProfiles: ManyToMany<typeof Profile>
 }
