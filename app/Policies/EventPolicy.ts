d9a05b183e71729d834be95a53bb4a76c5bd1d04
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-03-05 10:22:30 +0100

    fix(events): update policy

--- app/Policies/EventPolicy.ts
+++ app/Policies/EventPolicy.ts
@@ -1,12 +1,13 @@
 import { BasePolicy } from '@ioc:Adonis/Addons/Bouncer'
 import { UserRole } from 'App/Enums/UserRole'
+import Event from 'App/Models/Event'
 import User from 'App/Models/User'
 
 export default class EventPolicy extends BasePolicy {
   public async before(user: User) {
     if (user.blockedAt) return false
 
-    if (user.role === UserRole.Admin) {
+    if (user.role === UserRole.SuperAdmin) {
       return true
     }
   }
@@ -19,15 +20,37 @@ export default class EventPolicy extends BasePolicy {
     return true
   }
 
-  public async create() {
-    return true
+  public async create(user: User) {
+    return (
+      user.role === UserRole.EventsCreator ||
+      user.role === UserRole.EventsManager ||
+      user.role === UserRole.Admin ||
+      user.role === UserRole.SuperEventsManager ||
+      user.role === UserRole.SuperSupervisor
+    )
   }
 
-  public async update() {
-    return true
+  public async update(user: User, event: Event) {
+    return (
+      user.id === event.creatorId ||
+      ((user.role === UserRole.EventsManager ||
+        user.role === UserRole.Supervisor ||
+        user.role === UserRole.Admin) &&
+        event.hostSchools.find((value) => value.id === user.profile.schoolId)) ||
+      user.role === UserRole.SuperEventsManager ||
+      user.role === UserRole.SuperSupervisor
+    )
   }
 
-  public async delete() {
-    return true
+  public async delete(user: User, event: Event) {
+    return (
+      user.id === event.creatorId ||
+      ((user.role === UserRole.EventsManager ||
+        user.role === UserRole.Supervisor ||
+        user.role === UserRole.Admin) &&
+        event.hostSchools.find((value) => value.id === user.profile.schoolId)) ||
+      user.role === UserRole.SuperEventsManager ||
+      user.role === UserRole.SuperSupervisor
+    )
   }
 }
