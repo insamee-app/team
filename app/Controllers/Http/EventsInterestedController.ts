cf37ef90e15ae509af0c6f3a787069a81eeb9ec6
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-03-24 19:48:12 +0100

    fix(events): unactivate participating when event is passed

--- app/Controllers/Http/EventsInterestedController.ts
+++ app/Controllers/Http/EventsInterestedController.ts
@@ -1,15 +1,20 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { EventProfileState } from 'App/Enums/EventProfileState'
+import Event from 'App/Models/Event'
 
 export default class EventsInterestedController {
   public async store({ response, auth, params }: HttpContextContract) {
     await auth.user?.load('profile')
 
-    await auth.user?.profile.related('events').sync({
-      [params.id]: {
-        state: EventProfileState.Interested,
-      },
-    })
+    const event = await Event.query().where('id', params.id).firstOrFail()
+
+    if (!event.isPassed()) {
+      await auth.user?.profile.related('events').sync({
+        [params.id]: {
+          state: EventProfileState.Interested,
+        },
+      })
+    }
 
     return response.redirect().toRoute('EventsController.show', { id: params.id })
   }
--- app/Controllers/Http/EventsParticipateController.ts
+++ app/Controllers/Http/EventsParticipateController.ts
@@ -1,15 +1,20 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { EventProfileState } from 'App/Enums/EventProfileState'
+import Event from 'App/Models/Event'
 
 export default class EventsParticipateController {
   public async store({ response, auth, params }: HttpContextContract) {
     await auth.user?.load('profile')
 
-    await auth.user?.profile.related('events').sync({
-      [params.id]: {
-        state: EventProfileState.Participate,
-      },
-    })
+    const event = await Event.query().where('id', params.id).firstOrFail()
+
+    if (!event.isPassed()) {
+      await auth.user?.profile.related('events').sync({
+        [params.id]: {
+          state: EventProfileState.Participate,
+        },
+      })
+    }
 
     return response.redirect().toRoute('EventsController.show', { id: params.id })
   }
