6f610d10b12962792796317ff4d99a3753ac970f
Esteban S <esteban.soubiran@insa-cvl.fr>
2022-03-24 19:55:01 +0100

    fix(events): unactivate participating when event is passed

--- app/Controllers/Http/EventsInterestedController.ts
+++ app/Controllers/Http/EventsInterestedController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { EventProfileState } from 'App/Enums/EventProfileState'
+import { EventStatus } from 'App/Enums/EventStatus'
 import Event from 'App/Models/Event'
 
 export default class EventsInterestedController {
@@ -8,7 +9,7 @@ export default class EventsInterestedController {
 
     const event = await Event.query().where('id', params.id).firstOrFail()
 
-    if (!event.isPassed()) {
+    if (!event.isPassed() && event.status === EventStatus.Published) {
       await auth.user?.profile.related('events').sync({
         [params.id]: {
           state: EventProfileState.Interested,
@@ -22,7 +23,11 @@ export default class EventsInterestedController {
   public async destroy({ response, auth, params }: HttpContextContract) {
     await auth.user?.load('profile')
 
-    await auth.user?.profile.related('events').detach([params.id])
+    const event = await Event.query().where('id', params.id).firstOrFail()
+
+    if (!event.isPassed() && event.status === EventStatus.Published) {
+      await auth.user?.profile.related('events').detach([params.id])
+    }
 
     return response.redirect().toRoute('EventsController.show', { id: params.id })
   }
--- app/Controllers/Http/EventsParticipateController.ts
+++ app/Controllers/Http/EventsParticipateController.ts
@@ -1,5 +1,6 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { EventProfileState } from 'App/Enums/EventProfileState'
+import { EventStatus } from 'App/Enums/EventStatus'
 import Event from 'App/Models/Event'
 
 export default class EventsParticipateController {
@@ -8,7 +9,7 @@ export default class EventsParticipateController {
 
     const event = await Event.query().where('id', params.id).firstOrFail()
 
-    if (!event.isPassed()) {
+    if (!event.isPassed() && event.status === EventStatus.Published) {
       await auth.user?.profile.related('events').sync({
         [params.id]: {
           state: EventProfileState.Participate,
@@ -22,7 +23,11 @@ export default class EventsParticipateController {
   public async destroy({ response, auth, params }: HttpContextContract) {
     await auth.user?.load('profile')
 
-    await auth.user?.profile.related('events').detach([params.id])
+    const event = await Event.query().where('id', params.id).firstOrFail()
+
+    if (!event.isPassed() && event.status === EventStatus.Published) {
+      await auth.user?.profile.related('events').detach([params.id])
+    }
 
     return response.redirect().toRoute('EventsController.show', { id: params.id })
   }
